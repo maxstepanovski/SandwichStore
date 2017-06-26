aaf9dc2b2755f03135b8b0f704396e6c12185b5e
Max Stepanovski <stepanov_maks@mail.ru>
2017-06-26 10:55:35 +0300

    Initial commit
    StructuralPatternsTraining

---
+++ app/src/main/java/com/mambayamba/sandwichstore/MainActivity.java
@@ -0,0 +1,25 @@
+package com.mambayamba.sandwichstore;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
